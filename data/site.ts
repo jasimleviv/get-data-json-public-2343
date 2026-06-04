1a93af9feeafc429cdca3243e966aa7cd218db12
unknown <jasimleviv@gmail.com>
2026-06-04 10:34:55 -0400

    '.'

--- data/site.ts
+++
@@ -1,8 +0,0 @@
-import siteData from "./site.json";
-
-export const siteConfig = siteData.siteConfig;
-export const navigationLinks = siteData.navigationLinks;
-export const breadcrumbs = siteData.breadcrumbs;
-export const webpageJsonLd = siteData.webpageJsonLd;
-export const faqJsonLd = siteData.faqJsonLd;
-export const breadcrumbJsonLd = siteData.breadcrumbJsonLd;
